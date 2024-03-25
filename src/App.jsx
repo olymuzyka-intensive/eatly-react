c4f13259924cda873a73ff10c6aaec86f35332c4
olymuzyka-intensive <137098489+olymuzyka-intensive@users.noreply.github.com>
2024-03-26 00:33:53 +0300

    install scss add in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import Home from "./pages/Home"
 import Menu from "./pages/Menu"
+import Blog from "./pages/Blog"
 import AboutUs from "./pages/AboutUs"
 import Contact from "./pages/Contact"
 import Cart from "./pages/Cart"
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/menu" element={<Menu/>}/>
+        <Route path="/blog" element={<Blog/>}/>
         <Route path="/aboutUs" element={<AboutUs/>}/>
         <Route path="/contact" element={<Contact/>}/>
         <Route path="/cart" element={<Cart/>}/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import './css/style.css'
+import './css/media.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 )
