13a47a451225d4487e2014c2770e397c4998f7a6
olymuzyka-intensive <137098489+olymuzyka-intensive@users.noreply.github.com>
2024-06-10 12:10:34 +0300

    fix valid

--- src/pages/Sign.jsx
+++ src/pages/Sign.jsx
@@ -1,7 +1,32 @@
+import React, { useState } from 'react';
 import { Link } from "react-router-dom"
 
 
 function Sign() {
+        const [name, setName] = useState('');
+        const [email, setEmail] = useState('');
+        const [phone, setPhone] = useState('');
+        const [isValid, setIsValid] = useState(false);
+      
+        const handleSubmit = (e) => {
+          e.preventDefault();
+          const testEmail = /\S+@\S+\.\S+/;
+          const testPhone = /^\+?(375)\s?\-?\(?(29|25|44|33|17)\)?\s?-?[0-9]\s?-?[0-9]\s?-?[0-9]\s?-?[0-9]\s?-?[0-9]\s?-?[0-9]\s?-?[0-9]$/g;
+  
+          if (name && email && phone) {
+            if ((testEmail.test(email)) && (testPhone.test(phone))) {
+                setIsValid(true);
+            }
+                        
+            setName('');
+            setEmail('');
+            setPhone('');
+          } else {
+            setIsValid(false);
+          }
+        };
+    
+
     return (
     <>
         <div className="popup">
@@ -13,16 +38,21 @@ function Sign() {
                                 <img src="/src/img/Logo.svg" alt="Logo"/>
                             </Link>
                         </div>
-                        <form className="popup__left_form">
+                        <form className="popup__left_form" onSubmit={handleSubmit}>
                             <div className="popup__left_title">
                                 Sign Up To eatly
                             </div>
-                            <input type="text" placeholder="Full Name"></input>
-                            <input type="email" placeholder="Email"></input>
-                            <input type="number" placeholder="Phone" max="15" min="1"></input>
-                            {/* <Link to="/#" className="btn btn--confirm">confirm</Link> */}
+                            <input type="text" placeholder="Full Name" value={name} required
+                                onChange={(e) => setName(e.target.value)}
+                                style={{ borderColor: isValid || !name ? 'none' : '#6C5FBC' }}/>
+                            <input type="email" placeholder="Email" value={email} required
+                                onChange={(e) => setEmail(e.target.value)} 
+                                style={{ borderColor: isValid || !email ? 'none' : '#6C5FBC' }}/>
+                            <input type="tel" placeholder="Phone" value={phone} required
+                                onChange={(e) => setPhone(e.target.value)} 
+                                style={{ borderColor: isValid || !phone ? 'none' : '#6C5FBC' }}/>
                             <button className="btn btn--confirm">confirm</button>
-
+                              {isValid && <div className='form__field'>We will contact you as soon as possible</div>}
                         </form>
                     </div>
                     <div className="popup__right">
